b0d73a2a82c733747750b12485cedeeca26ffe91
lzhm216@163.com <lzhm216@163.com>
2019-10-09 23:22:41 +0800

    add login test

---
+++ LoginTest/app/src/main/java/com/gcy/www/logintest/ui/login/MainActivity.java
@@ -0,0 +1,29 @@
+package com.gcy.www.logintest.ui.login;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.gcy.www.logintest.R;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"main", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
